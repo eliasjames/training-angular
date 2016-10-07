17dec3f4ee798ef7ced26363b143983af2b33edd
Elias Carlston <elias@eliasjames.com>
2016-10-06 23:13:26 -0500

    New song form

--- course-materials/examples/1.2/songsController.js
+++ course-materials/examples/1.2/songsController.js
@@ -6,4 +6,12 @@ songsApp.controller( 'songsController', function songsControllerInit( $scope ) {
     { genre: 'Blues', title: 'Rock Me', artist: 'Muddy Waters' },
     { genre: 'Rock', title: '25 or 6 to 4', artist: 'Chicago' }
   ];
+
+  this.startNewSong = function startNewSong() {
+    this.newSong = {};
+  };
+  this.finishNewSong = function finishNewSong() {
+    this.songs.push( this.newSong );
+    delete this.newSong;
+  }
 });
