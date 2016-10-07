f4ae8683eb5d2d0e51797c03465a589f874d31bf
Elias Carlston <elias@eliasjames.com>
2016-10-06 20:48:14 -0500

    demo ngRepeat strict equality problem & solution

--- course-materials/examples/1.2/songsController.js
+++ course-materials/examples/1.2/songsController.js
@@ -1,6 +1,8 @@
 songsApp.controller( 'songsController', function songsControllerInit( $scope ) {
+    var demoStrictEquality = { genre: 'Post rock', title: 'TNT', artist: 'Tortoise' };
   this.songs = [
-    { genre: 'Post rock', title: 'TNT', artist: 'Tortoise' },
+    demoStrictEquality,
+    demoStrictEquality,
     { genre: 'Blues', title: 'Rock Me', artist: 'Muddy Waters' },
     { genre: 'Rock', title: '25 or 6 to 4', artist: 'Chicago' }
   ];
